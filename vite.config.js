699de2adade246679df6ef5480840c3d05d0af80
mohanawins <mohanakaraiyandi@gmail.com>
2025-08-19 20:34:43 +0530

    Switch to HashRouter for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/winebar-clone/", // 👈 very important for GitHub Pages
-  build: {
-    outDir: "dist", // default build output
-  },
+  base: "/winebar-clone/",   // ✅ must match your repo name
 });
