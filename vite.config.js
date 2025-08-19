9d418e7c644007ea7d2340d0e8ef89344caa1134
mohanawins <mohanakaraiyandi@gmail.com>
2025-08-19 19:56:46 +0530

    Fix GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base: '/winebar-clone/',  // 👈 EXACT repo name from GitHub
-=======
-  base: '/winebar-clone/',   // must match repo name exactly
->>>>>>> 56919609c9996e1473c7c53680b7e491d0971fdf
+  base: '/winebar-clone/',   // 👈 must match your repo name exactly
 })
