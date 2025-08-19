cc21a99d1eb5a5b74d4301c03a683be3e3aaa837
mohanawins <mohanakaraiyandi@gmail.com>
2025-08-19 20:08:28 +0530

    Fix vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/winebar-clone/',   // 👈 must match your repo name exactly
+  base: '/winebar-clone/',   // must match repo name!
 })
